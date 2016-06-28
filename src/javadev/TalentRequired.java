2c26ce51f09cb209cc575fdf873a62065be35e68
reve99 <m.elfeky@3ddx.com>
2016-06-28 13:21:43 +0200

    June 28, 2016 Java Developer Job Post

---
+++ src/javadev/TalentRequired.java
@@ -0,0 +1,69 @@
+/*
+ *Developed by Mohamed El-Feky @Reve99
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javadev;
+
+
+
+public class TalentRequired {
+
+    public enum Essentials {
+        JavaSE,JavaEE,HTML,CSS,SQL
+    };
+    
+    public enum Prefared {
+        XML,JavaScript,JQuery,GWT,Vaadin,MySQL
+    };
+    public enum Knowledge {
+        WebService,ORM,OOP,Design_Patterns,Application_Design
+    };
+    
+    public static void main(String[] args) {
+        
+        TalentRequired talent = new TalentRequired();
+        if(talent.gotEssentials() && talent.gotKnowledgeSet()){
+            
+            if(talent.gotPrefared()){
+                
+                if(talent.getMinimumExperiance() >1 && talent.getMinimumExperiance() <5)
+                    
+                    if(talent.sendCV())System.out.println("Hello 3D | Diagnostix");
+            }
+        } 
+        
+        
+    }
+    public boolean sendCV(){
+        //Kindly Send us your C.v to 
+        String mail = "recruitment@3ddx.com";
+        return true;
+    }
+    
+    public int getMinimumExperiance(){
+        int minimumExp = 0;//change to your actual Minimum Experiance
+        return minimumExp;
+    }
+    
+    public boolean gotEssentials(){
+        //DO you have Essentian Requirements for the Job
+        boolean essentials = false;//change to true if you do 
+        return essentials;
+    }
+    
+    public boolean gotPrefared(){
+        //DO you have Prefared  Requirements for the Job
+        boolean prefared = false;//change to true if you have at least one 
+        return prefared;
+    }
+    
+    public boolean gotKnowledgeSet(){
+        //DO you have Those Knowledge Set for the Job
+        boolean iGotKnowledge = false;//change to true if you do 
+        return iGotKnowledge;
+    }
+    
+}
